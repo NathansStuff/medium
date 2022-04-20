a75f8f3d19e05884893e2da4266b4abcae922afd
NathansStuff <n.odonnell@hotmail.com>
2022-04-20 22:06:11 +1000

    spanner component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/header.components'
+import Spanner from '../components/spanner.components'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+      <Spanner />
     </div>
   )
 }
